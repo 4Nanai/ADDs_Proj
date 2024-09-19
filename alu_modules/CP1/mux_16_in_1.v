5e642e88fc6d12ec0a02cc01054f5e0cd4ad4cc2
Chitose Ri <chitose@Chitoses-MacBook-Pro.local>
2024-09-19 17:32:20 -0400

    Add: CheckPoint 2

--- alu_modules/CP1/mux_16_in_1.v
+++ alu_modules/CP1/mux_16_in_1.v
@@ -3,14 +3,14 @@ module mux_16_in_1(in0, in1, s, out);
 	input s;
 	output [15:0] out;
 	
-	mux_4_in_1 mux1(
+	mux_8_in_1 mux1(
 		.in0(in0[7:0]),
 		.in1(in1[7:0]),
 		.s(s),
 		.out(out[7:0])
 	);
 	
-	mux_4_in_1 mux2(
+	mux_8_in_1 mux2(
 		.in0(in0[15:8]),
 		.in1(in1[15:8]),
 		.s(s),
